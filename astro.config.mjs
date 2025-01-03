fa0038a46ca2e8e50f3954191f77c36835867d8d
Abdurrahim <ebdurrehim5@gmail.com>
2025-01-03 23:40:33 +0100

    feat: integrate remark-math and rehype-katex for enhanced math rendering in MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,26 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ahmadov.tech',
-	integrations: [mdx(), sitemap(), react(), tailwind()],
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkMath],
+			rehypePlugins: [[rehypeKatex, {
+				// KaTeX options
+				strict: false,
+				macros: {
+					"\\bmatrix": "\\begin{bmatrix}#1\\end{bmatrix}",
+				}
+			}]]
+		}), 
+		sitemap(), 
+		react(), 
+		tailwind()
+	],
 	base:'/'
 });
